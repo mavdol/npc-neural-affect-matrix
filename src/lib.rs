4004978628df1fda1e911bf84750e1d4e2a34fa4
mohamed-mosaiqart <mdiallo.av@gmail.com>
2025-09-26 16:27:09 +0200

    delete unsused model export

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
-pub mod models;
 pub mod config;
 pub mod modules;
 pub mod utils;
 pub mod api;
 
-pub use models::input::InputData;
+
 pub use config::{NpcConfig, Identity, PersonalityTraits, MemoryConfig};
 
 pub use modules::memory::{MemoryEmotionEvaluator};
